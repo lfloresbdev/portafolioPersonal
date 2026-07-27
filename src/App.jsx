3a433018d0661ce0a87bbec55c1bd4f1909349a3
lfloresbdev <lfloresb.work@gmail.com>
2026-07-26 19:44:19 -0500

    Configuración shadcn ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import { Button } from '@/components/ui/button'
 
 function App() {
   return (
     <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-5xl font-bold">
-        Portfolio Website
-      </h1>
+      <Button>Mi Portafolio</Button>
     </div>
   );
 }
